0fc0309ee2f89c783a1fb68fb96bee28e02e8248
Fabien Herfray <herfray.fabien@gmail.com>
2017-12-29 17:50:50 +0100

    Updated

---
+++ components/queue_repo.go
@@ -0,0 +1,138 @@
+package components
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"text/template"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"github.com/solher/toolbox/sql/types"
+)
+
+var getPostgresQueueTmpl = template.Must(template.New("get_postgres_queue").Parse(`
+		SELECT id, object_id, retries, created_at
+		FROM {{.Table}}
+		WHERE TRUE
+		{{if .FromID -}}
+		AND id > :from_id
+		{{end -}}
+		ORDER BY id ASC
+		LIMIT :id_limit
+	`))
+
+// Task is a standardised queued task.
+type Task struct {
+	ID        uint64    `json:"id"        db:"id"`
+	ObjectID  uint64    `json:"objectId"  db:"object_id"`
+	Retries   uint64    `json:"retries"   db:"retries"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+	Err       error
+}
+
+// QueueRepository allows interaction with a Task queue.
+type QueueRepository interface {
+	GetQueue(ctx context.Context, table string, fromID uint64, limit int) (tasks []Task, err error)
+	RetryTasks(ctx context.Context, table string, fromID uint64) error
+	DeleteFromQueue(ctx context.Context, table string, id uint64) error
+	UpdateTask(ctx context.Context, table string, id, retries uint64) error
+	InsertTask(ctx context.Context, table string, task *Task) error
+}
+
+// NewPostgresQueueRepo returns a new Postgres backed QueueRepository.
+func NewPostgresQueueRepo(db *sqlx.DB) QueueRepository {
+	return &postgresQueueRepo{db: db}
+}
+
+type postgresQueueRepo struct {
+	db *sqlx.DB
+}
+
+func (r *postgresQueueRepo) GetQueue(ctx context.Context, table string, fromID uint64, limit int) (tasks []Task, err error) {
+	if limit == 0 {
+		return tasks, nil
+	}
+
+	arg := &struct {
+		FromID  types.NullUint64 `db:"from_id"`
+		IDLimit int              `db:"id_limit"`
+		Table   string
+	}{
+		FromID:  types.NullUint64(fromID),
+		IDLimit: limit,
+		Table:   table,
+	}
+	query := bytes.NewBuffer(nil)
+	if err := getPostgresQueueTmpl.Execute(query, arg); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	stmt, err := r.db.PrepareNamed(query.String())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer stmt.Close()
+
+	if err := stmt.SelectContext(ctx, &tasks, arg); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return tasks, nil
+}
+
+func (r *postgresQueueRepo) RetryTasks(ctx context.Context, table string, fromID uint64) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		fmt.Sprintf(`
+		WITH t AS (
+			SELECT id, object_id, retries
+			FROM %[1]s
+			WHERE id < $1
+			AND created_at < NOW() - INTERVAL '10 minutes'
+		), d AS (
+      DELETE FROM %[1]s WHERE id IN (SELECT id FROM t)
+		)
+		INSERT INTO %[1]s (object_id, retries) SELECT t.object_id, t.retries FROM t ON CONFLICT (object_id) DO NOTHING 
+		`, table),
+		fromID,
+	)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (r *postgresQueueRepo) DeleteFromQueue(ctx context.Context, table string, id uint64) error {
+	if _, err := r.db.ExecContext(ctx, fmt.Sprintf("DELETE FROM %s WHERE id = $1", table), id); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (r *postgresQueueRepo) UpdateTask(ctx context.Context, table string, id, retries uint64) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		fmt.Sprintf("UPDATE %s SET retries = $1, created_at = CURRENT_TIMESTAMP WHERE id = $2", table),
+		retries,
+		id,
+	)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (r *postgresQueueRepo) InsertTask(ctx context.Context, table string, task *Task) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		fmt.Sprintf("INSERT INTO %s (object_id, retries) VALUES ($1, $2) ON CONFLICT (object_id) DO NOTHING", table),
+		task.ObjectID,
+		task.Retries,
+	)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
---
+++ components/queue_worker.go
@@ -0,0 +1,83 @@
+package components
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-kit/kit/log"
+)
+
+// LoggerWithTask adds task information to a logger.
+func LoggerWithTask(logger log.Logger, task *Task) log.Logger {
+	return log.With(
+		logger,
+		"id", task.ID,
+		"objectId", task.ObjectID,
+		"retries", task.Retries,
+	)
+}
+
+// NewQueueWorker returns a worker that synchronizes a SQL table formed as a list of pair documentID/timestamp with a channel.
+func NewQueueWorker(l log.Logger, repo QueueRepository, table string, syncTick time.Duration, outCh chan<- Task, inCh <-chan Task) Worker {
+	return NewWorker(
+		l,
+		&queueWorker{
+			table:  table,
+			repo:   repo,
+			ticker: time.NewTicker(syncTick),
+			outCh:  outCh,
+			inCh:   inCh,
+		},
+	)
+}
+
+type queueWorker struct {
+	table  string
+	repo   QueueRepository
+	ticker *time.Ticker
+	lastID uint64
+	outCh  chan<- Task
+	inCh   <-chan Task
+}
+
+func (w *queueWorker) Name() string {
+	return w.table
+}
+
+func (w *queueWorker) Work(ctx context.Context, l log.Logger) {
+	select {
+	case <-w.ticker.C:
+		if err := w.repo.RetryTasks(ctx, w.table, w.lastID); err != nil {
+			l.Log("err", err)
+			return
+		}
+		tasks, err := w.repo.GetQueue(ctx, w.table, w.lastID, cap(w.outCh)-len(w.outCh))
+		if err != nil {
+			l.Log("err", err)
+			return
+		}
+		for _, task := range tasks {
+			w.outCh <- task
+		}
+		if len(tasks) > 0 {
+			w.lastID = tasks[len(tasks)-1].ID
+		}
+	case task := <-w.inCh:
+		if task.Err != nil && task.Retries < 5 {
+			task.Retries++
+			if err := w.repo.UpdateTask(ctx, w.table, task.ID, task.Retries); err != nil {
+				l.Log("err", err)
+				return
+			}
+			return
+		}
+		if err := w.repo.DeleteFromQueue(ctx, w.table, task.ID); err != nil {
+			l.Log("err", err)
+			return
+		}
+	case <-ctx.Done():
+		return
+	}
+}
+
+func (w *queueWorker) Shutdown(ctx context.Context, l log.Logger) error { return nil }
