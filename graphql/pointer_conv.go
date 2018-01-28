4f9df13c5ce0f530cf7335b7872da076db47fee4
Fabien Herfray <herfray.fabien@gmail.com>
2018-01-28 13:01:15 +0100

    Updated

--- graphql/pointer_conv.go
+++ graphql/pointer_conv.go
@@ -1,9 +1,19 @@
 package graphql
 
-// PointerToStringArray converts a pointer to an array of string.
+import "github.com/solher/toolbox/graphql/types"
+
+// PointerToStringArray converts a pointer to an array of strings.
 func PointerToStringArray(ptr *[]string) []string {
 	if ptr != nil {
 		return *ptr
 	}
 	return []string{}
+}
+
+// PointerToIDArray converts a pointer to an array of ids.
+func PointerToIDArray(ptr *[]types.ID) []types.ID {
+	if ptr != nil {
+		return *ptr
+	}
+	return []types.ID{}
 }
