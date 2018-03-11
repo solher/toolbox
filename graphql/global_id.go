f41a0d97ed6a231183bb1f6c326b06127c3e010b
Fabien Herfray <herfray.fabien@gmail.com>
2018-03-11 13:44:26 +0100

    Not using base64 anymore

--- graphql/global_id.go
+++ graphql/global_id.go
@@ -1,7 +1,6 @@
 package graphql
 
 import (
-	"encoding/base64"
 	"fmt"
 	"strconv"
 
@@ -12,20 +11,20 @@ import (
 // "global ID" that is unique among all types. ObjCode is two characters.
 func ToGlobalID(objCode string, id uint64) types.ID {
 	str := fmt.Sprintf("%s%d", objCode, id)
-	encStr := base64.StdEncoding.EncodeToString([]byte(str))
-	return types.ID(encStr)
+	// encStr := base64.StdEncoding.EncodeToString([]byte(str))
+	return types.ID(str)
 }
 
 // FromGlobalID takes the "global ID" created by toGlobalID, and returns the type name and ID
 // used to create it. ObjCode is two characters.
 func FromGlobalID(globalID types.ID) (objCode string, id uint64) {
-	idStr, err := base64.StdEncoding.DecodeString(string(globalID))
-	if err != nil || len(idStr) < 3 {
+	// idStr, err := base64.StdEncoding.DecodeString(string(globalID))
+	if len(globalID) < 3 {
 		return "", 0
 	}
-	id, err = strconv.ParseUint(string(idStr[2:]), 10, 64)
+	id, err := strconv.ParseUint(string(globalID[2:]), 10, 64)
 	if err != nil {
 		return "", 0
 	}
-	return string(idStr[:2]), id
+	return string(globalID[:2]), id
 }
