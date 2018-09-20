27a80137192ddcfe07d40fce155df2b386e0d9f1
Fabien Herfray <herfray.fabien@gmail.com>
2018-09-20 23:28:38 +0200

    XML support added

---
+++ api/xml.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"context"
+	"encoding/xml"
+	"errors"
+	"net/http"
+
+	"github.com/go-kit/kit/log"
+
+	"github.com/solher/toolbox"
+)
+
+// NewXML returns a new XML instance.
+func NewXML(logger log.Logger, debug bool) *XML {
+	if logger == nil {
+		logger = log.NewNopLogger()
+	}
+	return &XML{
+		logger: logger,
+		debug:  debug,
+	}
+}
+
+// XML returns a new XML encoder that logs errors.
+// If debug is set, it sets the stacktrace into the HTTP body responses.
+type XML struct {
+	logger log.Logger
+	debug  bool
+}
+
+// RenderError renders a HTTPError and logs it if it's a 500.
+func (x *XML) RenderError(ctx context.Context, w http.ResponseWriter, httpError HTTPError, e error) {
+	if e == nil {
+		e = errors.New("null")
+	}
+	location, _ := toolbox.HasStack(e)
+
+	if x.debug || (httpError.Status >= 500 && httpError.Status < 600) {
+		toolbox.LoggerWithRequestContext(ctx, x.logger).Log("status", httpError.Status, "err", e)
+	}
+
+	if x.debug {
+		x.renderXML(w, httpError.Status, &DebugHTTPError{
+			HTTPError: httpError,
+			Err:       e.Error(),
+			Location:  location,
+		})
+	} else {
+		x.renderXML(w, httpError.Status, &httpError)
+	}
+}
+
+// Render renders an object to XML.
+func (x *XML) Render(ctx context.Context, w http.ResponseWriter, status int, object interface{}) {
+	if object == nil {
+		w.WriteHeader(status)
+	} else {
+		x.renderXML(w, status, object)
+	}
+}
+
+func (x *XML) renderXML(w http.ResponseWriter, status int, object interface{}) {
+	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	w.WriteHeader(status)
+	xml.NewEncoder(w).Encode(object)
+}
